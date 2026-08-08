85533c8079a42702912e176be22afaef043338f2
raghuvindersingh <raghuvinderrana95@gmail.com>
2026-08-08 22:47:21 +0530

    Create Q2.c

---
+++ week1/Q2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main() {
+    int a[100], n, i, j, count;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    printf("Enter elements: ");
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+    for (i = 0; i < n; i++) {
+        count = 1;
+        if (a[i] == -99)
+            continue;
+        for (j = i + 1; j < n; j++) {
+            if (a[i] == a[j]) {
+                count++;
+                a[j] = -99;
+            }
+        }
+        printf("%d occurs %d times\n", a[i], count);
+    }
+    return 0;
+}
